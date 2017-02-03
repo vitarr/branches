4096fb2f97605931e0ec2d0f649933bc3a82933e
sasha44 <12345@i.ua>
2017-02-03 15:39:10 +0200

    created auth form

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Login</title>
+    </head>
+    <body>
+        <h1>Welcome, get log in.</h1>
+        <form action="auth.php" method="post">
+            <input type="text" name="login" placeholder="login"/>
+            <input type="password" name="password" placeholder="pass"/>
+            <input type="submit" value="log in"/>
+        </form>
+    </body>
+</html>
